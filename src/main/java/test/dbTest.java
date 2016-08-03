2be26465fc8184ff87d79d7c0992ee97eac3e752
HyeongJuKim <gudwndhkd@nate.com>
2016-08-03 21:45:58 +0900

    JDBC테스트 프로세스 코드 작성

--- src/main/java/test/dbTest.java
+++ src/main/java/test/dbTest.java
@@ -1,7 +1,5 @@
 package test;
 
-import static org.junit.Assert.*;
-
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -16,29 +14,59 @@ public class dbTest extends TestCase {
 	
 	@Test
 	public void testName() {
-			Connection con = null;
 
+			String sql = "SHOW DATABASES";
+			ResultSetProcess resultSetProcess = new PrintResult();
+			
+			getList(sql, resultSetProcess)
+			;
+			
+	}
+
+	private void getList(String sql, ResultSetProcess resultSetProcess) {
+		Connection con = null;			
+		
+		try {
+			con = (Connection) DriverManager.getConnection("jdbc:mysql://rkdgusrnrlrl.vps.phps.kr:3306/MEMO?haracterEncoding=utf-8",
+					"hj", "hj");
+			
+			java.sql.Statement st = null;
+			ResultSet rs = null;
+			st = con.createStatement();
+			rs = st.executeQuery(sql);
+			resultSetProcess.process(rs);
+
+		} catch (SQLException e) {
+			fail("sql에 문제가 있어");
+			
+		}
+	}
+	
+	//
+	interface ResultSetProcess<T> {
+		public T process(ResultSet resultSet); 
+		
+	}
+	
+	//
+	class PrintResult implements ResultSetProcess<Void>{
+
+		public Void process(ResultSet resultSet) {
+			String str;
 			try {
-				con = (Connection) DriverManager.getConnection("jdbc:mysql://rkdgusrnrlrl.vps.phps.kr:3306/MEMO?haracterEncoding=utf-8",
-						"hj", "hj");
 				
-				java.sql.Statement st = null;
-				ResultSet rs = null;
-				st = con.createStatement();
-				
-				rs = st.executeQuery("SHOW DATABASES");
-
-				while (rs.next()) {
-					String str = rs.getNString(1);
+				while (resultSet.next()) {
+					str = resultSet.getNString(1);
 					System.out.println(str);
 				}
-
-			} catch (SQLException e) {
-				fail("sql에 문제가 있어");
 				
+			} catch (SQLException e) {
+				e.printStackTrace();
 			}
 			
-			
+			return null;
+		}
+		
 	}
 	
 }
